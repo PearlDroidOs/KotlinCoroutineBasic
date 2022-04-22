638ae6c4354102e646865af38b5080d4518da240
PearlDroidOs <pearl.droidos@gmail.com>
2022-04-22 14:58:52 +0700

    Learning flow

---
+++ src/main/kotlin/Flow/Buffering.kt
@@ -0,0 +1,26 @@
+package Flow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    runBlocking {
+        val time = measureTimeMillis {
+            generate().buffer().collect {
+                delay(300L)
+                println(it)
+            }
+        }
+        println("Time: $time")
+    }
+}
+
+fun generate() = flow {
+    (1..10).forEach {
+        delay(100L)
+        emit(it)
+    }
+}
---
+++ src/main/kotlin/Flow/ExceptionHandling.kt
@@ -0,0 +1,43 @@
+package Flow
+
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        onCompletionOperator()
+    }
+}
+
+suspend fun onCompletionOperator() {
+    (1..3).asFlow()
+//        .onEach { check(it != 2) }
+        .onCompletion { cause ->
+            if(cause != null)
+                println("Flow completed with $cause")
+            else
+                println("Flow completed successfully")
+        }
+        .catch { e -> println("Caught exception $e") }
+        .collect { println(it) }
+
+}
+suspend fun catchOperator() {
+    (1..3).asFlow()
+        .onEach { check(it != 2) }
+        .catch { e -> println("Exception: $e") }
+        .collect { println(it) }
+}
+
+suspend fun tryCatch() {
+    try {
+        (1..3).asFlow()
+            .onEach { check(it != 2) }
+            .collect { println(it) }
+    } catch (ex: Exception) {
+        println("Exception: $ex")
+    }
+}
